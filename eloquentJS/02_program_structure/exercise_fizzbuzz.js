80081729385eab985944cb5eaf6cc56143e4f0b2
ibrahimbayyinah <ibrahimbayyinah@mail.com>
2020-12-20 11:26:14 +0100

    Created exercise_fizzbuss.js

---
+++ eloquentJS/02_program_structure/exercise_fizzbuzz.js
@@ -0,0 +1,15 @@
+// Exercise FizzBuzz
+for (let i = 1; i <= 100; i++) {
+
+  let word;
+  
+  if (i % 3 === 0 && i % 5 === 0) {
+    word = "FizzBuzz";
+  } else if (i % 3 === 0) {
+    word = "Fizz";
+  } else if (i % 5 === 0) {
+    word = "Buzz";
+  }
+  
+  console.log(word || i);
+}
